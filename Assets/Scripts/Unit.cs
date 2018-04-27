e8bbbcf0306cab577ab7627f5174d8e0027abd6b
Cooperjpk <cooperjpklassen@gmail.com>
2018-04-26 22:57:05 -0700

    Looking into read/write CSV files

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : Interactable
+{
+
+    public enum States
+    {
+        Idle,
+    }
+
+    public States currentState;
+
+    //Called every time a State is entered
+    void EnterState(States state)
+    {
+        switch (state)
+        {
+            case States.Idle:
+                {
+                    EnterIdleState();
+                    break;
+                }
+        }
+    }
+
+    //Called every update in a State
+    void UpdateState(States state)
+    {
+        switch (state)
+        {
+            case States.Idle:
+                {
+                    IdleState();
+                    break;
+                }
+        }
+    }
+
+    //Called every time a State is exitted
+    void ExitState(States state)
+    {
+        switch (state)
+        {
+            case States.Idle:
+                {
+                    ExitIdleState();
+                    break;
+                }
+        }
+    }
+
+    States DecideState()
+    {
+        //Logic goes here
+        //otherwise
+        return States.Idle;
+    }
+
+    void Start()
+    {
+        //Invoke the function that runs the behaviour for entering the intial currentState
+        Invoke("Enter" + currentState.ToString() + "State", 0);
+    }
+
+    void Update()
+    {
+        //Use logic to find out the current state
+        States newState = DecideState();
+
+        //If the newState was not the same as the old state then play the exit state of the old state and enter state of the new state 
+        if (newState != currentState)
+        {
+            ExitState(currentState);
+            currentState = newState;
+            EnterState(currentState);
+        }
+
+        //Play update state of the current state
+        UpdateState(currentState);
+    }
+
+    #region Idle
+    void EnterIdleState()
+    {
+
+    }
+
+    void IdleState()
+    {
+
+
+    }
+
+    void ExitIdleState()
+    {
+
+
+    }
+    #endregion
+
+}
